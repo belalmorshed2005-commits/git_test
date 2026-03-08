061fc67f4d7d490882ec65e11f22f536071c8567
belal morshed <belal.morshed.2005@gmail.com>
2026-03-08 22:39:41 +0300

    first project

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 int main(){
 int a=2;int k=0;// a is prime ?
 for(a=2;a<100;a++){
-    if(a==4) continue ;
-     for(int i=2;a>i*i;i++){
+    k=0;
+     for(int i=2;a>=i*i;i++){
         if(a%i==0){
            k+=1;
 
@@ -19,7 +19,7 @@ for(a=2;a<100;a++){
 
 
 
-k=0;
+
 }
 
 return 0;
